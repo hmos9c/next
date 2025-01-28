ca51066869290a5a691c9df8f75842db6b5a1899
hmos9c <hmos9c@gmail.com>
2025-01-28 17:26:34 +0700

    fix 0.3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -129,7 +129,10 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    throw new Error("Failed to Delete Invoice");
+  }
 }
